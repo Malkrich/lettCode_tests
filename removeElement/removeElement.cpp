360220c3914a3e5302fc968f21d2038082309ea7
Malkrich <mathurin.lemarie@gmail.com>
2022-07-21 11:33:34 +0200

    remove Elemet solved

---
+++ removeElement/removeElement.cpp
@@ -0,0 +1,17 @@
+#include "removeElement.hpp"
+
+int removeElement(vector<int>& nums, int val)
+{
+    int j = 0;
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(nums[i] != val)
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    return j;
+}
---
+++ removeElement/removeElement.hpp
@@ -0,0 +1,11 @@
+#ifndef REMOVEELEMENT_H
+#define REMOVEELEMENT_H
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int removeElement(vector<int>& nums, int val);
+
+#endif // REMOVEELEMENT_H
